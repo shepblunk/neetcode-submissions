5151bca5d4bc9440e4dbb3bf1e3eb95a43d7240f
shepblunk <aurelienmnl@gmail.com>
2026-04-09 17:49:04 +0200

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        if len(s)==1:
+            return False
+
+        hashmap = {
+            123 : 125,
+            91 : 93,
+            40 : 41
+        }    
+
+        stack=[]
+
+        for i in range(len(s)):
+            x = ord(s[i])
+            
+            if x in hashmap:
+                stack.append(x)
+
+            else:
+                if stack:
+                    last_element = stack.pop()
+
+                    if x != hashmap[last_element]:
+                        return False
+                else:
+                    return False
+
+    
+        return len(stack)==0
