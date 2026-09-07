e209d6abf71113fea6b72515c8da5b829ac563c1
shepblunk <aurelienmnl@gmail.com>
2026-09-07 16:39:20 +0200

    Add: fruit-into-baskets - submission-11

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-11.py
@@ -0,0 +1,49 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        L = 0
+        best = 0
+        hashmap = {}
+        length= len(fruits)
+
+        for R in range(length):
+            fruit_droite = fruits[R]
+            
+            hashmap[fruit_droite] = hashmap.get(fruit_droite,0)+1
+
+            while len(hashmap)>2:
+                fruit_gauche = fruits[L]
+                hashmap[fruit_gauche] -= 1
+                if hashmap[fruit_gauche]==0:
+                    del hashmap[fruit_gauche]
+                L +=1
+            best = max(best,R-L+1)
+        return best
+
+
+
+
+
+        """
+        L = 0
+        best = 0
+        hashmap = {}
+
+        for R in range(len(fruits)):
+            fruit_droite = fruits[R]
+            hashmap[fruit_droite] = hashmap.get(fruit_droite,0) + 1
+
+            while len(hashmap)>2:
+                fruit_gauche = fruits[L]
+                hashmap[fruit_gauche] -= 1
+                if hashmap[fruit_gauche] == 0:
+                    del hashmap[fruit_gauche]
+                L += 1
+
+            best = max(best,R-L+1)
+                
+        return best"""
+
+
+
+
+        
