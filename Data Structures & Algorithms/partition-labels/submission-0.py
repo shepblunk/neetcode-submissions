4c10621691847f3e8e5b5988c933ac35e998209d
shepblunk <aurelienmnl@gmail.com>
2026-09-21 15:30:01 +0200

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        hashmap = {}
+        res=[]
+        for i,char in enumerate(s):
+            hashmap[char] = i
+        print(hashmap)
+
+        size = 0
+        end = 0
+        for i,char in enumerate(s):
+            if char in hashmap:
+                size += 1
+                end=max(hashmap[char],end)
+
+            if i==end:
+                res.append(size)
+                size = 0
+
+        return res
